4f05ad94f108c16dda916058e59d77c8f75747d9
Permana Adi Cahya <87966233+permanaadicahya@users.noreply.github.com>
2021-09-29 10:59:22 +0700

    Add files via upload

---
+++ Pintek Revamp/Libs/CustomKeywords.groovy
@@ -0,0 +1,14 @@
+
+/**
+ * This class is generated automatically by Katalon Studio and should not be modified or deleted.
+ */
+
+import java.lang.String
+
+
+
+def static "ClickNumber.clickNumber"(
+    	String number	) {
+    (new ClickNumber()).clickNumber(
+        	number)
+}
