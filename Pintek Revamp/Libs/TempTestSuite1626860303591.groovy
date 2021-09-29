8a0457513188ab3fd26f52f4bf90f028b6f4ca00
Permana Adi Cahya <87966233+permanaadicahya@users.noreply.github.com>
2021-09-29 11:03:27 +0700

    Add files via upload

---
+++ Pintek Revamp/Libs/TempTestSuite1626860303591.groovy
@@ -0,0 +1,39 @@
+import com.kms.katalon.core.logging.KeywordLogger
+import com.kms.katalon.core.exception.StepFailedException
+import com.kms.katalon.core.reporting.ReportUtil
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.testdata.TestDataColumn
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import internal.GlobalVariable as GlobalVariable
+
+Map<String, String> suiteProperties = new HashMap<String, String>();
+
+
+suiteProperties.put('id', 'Test Suites/Pintek B2C Apply Partner')
+
+suiteProperties.put('name', 'Pintek B2C Apply Partner')
+
+suiteProperties.put('description', '')
+
+suiteProperties.put('stopImmediately', 'false')
+ 
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.cucumber.keyword.internal.CucumberDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.windows.keyword.contribution.WindowsDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
+
+
+
+RunConfiguration.setExecutionSettingFile("C:\\Users\\Yulia Dewi\\Katalon Studio\\Pintek BDD Project\\Reports\\20210721_163823\\Pintek B2C Apply Partner\\20210721_163823\\execution.properties")
+
+TestCaseMain.beforeStart()
+
+TestCaseMain.startTestSuite('Test Suites/Pintek B2C Apply Partner', suiteProperties, new File("C:\\Users\\Yulia Dewi\\Katalon Studio\\Pintek BDD Project\\Reports\\20210721_163823\\Pintek B2C Apply Partner\\20210721_163823\\testCaseBinding"))
